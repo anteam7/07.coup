a93a3dc763cabd5cdb9436fa8ab4e5e55dc7c9ba
shahn03 <anseunghyok@gmail.com>
2022-10-25 17:59:41 +0900

    upate product reg page

--- affenv/coup_project/cadmin/urls.py
+++ affenv/coup_project/cadmin/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 import cadmin.views
 
 urlpatterns = [
-    path('', cadmin.views.main)
+    path('', cadmin.views.product)
 ]
--- affenv/coup_project/cadmin/views.py
+++ affenv/coup_project/cadmin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main(request):
-    return render(request, 'admin/main.html')
+def product(request):
+    return render(request, 'admin/product.html')
